1f39b500f863f51fcc95fa6b0adc79862b221550
treaditup <roman.pozharnov@yandex.ru>
2025-05-05 15:12:21 +0300

    feat: reporter in docker

--- reporter/src/reporter/infrastructure/email/fake.py
+++ reporter/src/reporter/infrastructure/email/fake.py
@@ -12,7 +12,7 @@ class FakeEmailSender(ReportSender):
         self._reports_path = reports_path
 
     async def send(self, user: User, report: Report) -> None:
-        with open(self._reports_path / user.email / report.filename, "wb") as f:  # noqa: ASYNC230
+        with open(self._reports_path / f"{user.email}_{report.filename}", "wb") as f:  # noqa: ASYNC230
             f.write(report.payload)
 
         await asyncio.sleep(random.randint(1, 10) / 10)  # noqa: S311
